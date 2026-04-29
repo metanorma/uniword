d97592809c766585af54d516fc9a01bb2da83e2e
Ronald Tse <ronald.tse@ribose.com>
2026-04-29 23:47:05 +0800

    fix: header/footer round-trip, relationship id types, dedup rels
    - Extract header/footer parts from DOCX during load
    - Deduplicate content_types overrides and relationships during serialization
    - Preserve existing rIds when reconciling relationships
    - Add RelationshipId type for cross-namespace r:embed/r:link attributes
    - Use typed RelationshipId on Blip and Imagedata elements

--- lib/uniword/vml/imagedata.rb
+++ lib/uniword/vml/imagedata.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "lutaml/model"
+require "uniword/ooxml/types/relationship_id"
 
 module Uniword
   module Vml
@@ -11,6 +12,8 @@ module Uniword
     class Imagedata < Lutaml::Model::Serializable
       attribute :src, :string
       attribute :relid, :string
+      attribute :r_id, Ooxml::Types::RelationshipId
+      attribute :r_href, Ooxml::Types::RelationshipId
       attribute :title, :string
       attribute :croptop, :string
       attribute :cropbottom, :string
@@ -23,6 +26,8 @@ module Uniword
 
         map_attribute "src", to: :src
         map_attribute "relid", to: :relid
+        map_attribute "id", to: :r_id, render_nil: false
+        map_attribute "href", to: :r_href, render_nil: false
         map_attribute "title", to: :title
         map_attribute "croptop", to: :croptop
         map_attribute "cropbottom", to: :cropbottom
